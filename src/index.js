89fd043b9be27e23a7eaccb4c9041b870131f4cf
josh-everett01 <59103702+josh-everett01@users.noreply.github.com>
2020-07-07 19:46:01 -0500

    Completed HTML / responsive CSS / JS for Issue #1. I did have an issue with webpack and my JavaScript not linking with my HTML, but I went ahead and manually added my JavaScript file (which is stated not to do). I hope to get this cleared up soon.

--- src/index.js
+++ src/index.js
@@ -2,9 +2,32 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import './css/base.scss';
+// import './src/css/base.scss';
+// import './css'
 
-// An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// An example of how you tell webpack to use an image (also need to link to it in the index.html
 
-console.log('This is the JavaScript entry file - your code begins here.');
+// export
+
+var password = document.getElementById('input-password');
+var username = document.getElementById('input-username');
+var agentPw = 'travel2020';
+var agentUn = 'agency';
+
+function authPW() {
+  if (password.value == agentPw && username.value == agentUn) {
+    document.getElementById('site-container').style.display = "none";
+    var h = document.createElement("H1"); // Create the H1 element 
+    var t = document.createTextNode("AGENT"); // Create a text element 
+    h.appendChild(t); // Append the text node to the H1 element 
+
+    document.body.appendChild(h); // Append the H1 element to the document body 
+  } else {
+    alert("Incorrect Login!!");
+    document.getElementById("input-username").value = "";
+    document.getElementById("input-password").value = "";
+    
+  }
+
+  
+};
