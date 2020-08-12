7d777f245d93ecf44c6e7bda2c76ece74a33e706
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-08-12 18:36:33 -0500

    Add initial README.md file for Travel Tracker App

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: '/src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'main.bundle.js'
@@ -19,15 +19,15 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|gif)$/,
         use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: '[name].[ext]',
-              outputPath: 'images/',
-              publicPath: 'images/'
-            }
-          }
-        ]
+              {
+                loader: 'file-loader',
+                options: {
+                  name: '[name].[ext]',
+                  outputPath: 'images/',
+                  publicPath: 'images/'
+                }
+              }
+            ]
       }
     ],
   },
@@ -38,6 +38,6 @@ module.exports = {
     })
   ],
   devServer: {
-    contentBase: './dist'
+         contentBase: './dist'
   }
 };
