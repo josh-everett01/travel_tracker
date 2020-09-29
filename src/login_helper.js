e0f4a1603a7801a04e2e3296dec325d33b6254de
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-09-29 10:54:50 -0500

    Correct date issues
    This commit will correct the issue with the dates displaying incorrectly after the traveler selects the start / end dates. The issue was with time zone changes making it to where the date was always shown as a day behind.

--- src/login_helper.js
+++ src/login_helper.js
@@ -4,9 +4,11 @@ import {
   renderSuccessfulAgencyLogin,
   renderTrips,
   renderTotalSpent,
-  renderTripDate
 } from "./traveler_dashboard_helper";
-import { getAllDestinationsForAgent, getAllTripsForAgent } from "./travel_tracker_service";
+import {
+  getAllDestinationsForAgent,
+  getAllTripsForAgent,
+} from "./travel_tracker_service";
 
 function isValidPassword(passwordInput) {
   var correctPw = "travel2020";
@@ -69,19 +71,29 @@ function filterPastAndUpcomingTrips(travelerTrips) {
 
 function daysBetween(startDate, endDate) {
   let formattedStartDate = new Date(startDate);
+  formattedStartDate = new Date(formattedStartDate.getTime() + formattedStartDate.getTimezoneOffset() * 60000)
   let formattedEndDate = new Date(endDate);
+  formattedEndDate = new Date(formattedEndDate.getTime() + formattedEndDate.getTimezoneOffset() * 60000)
+
   // The number of milliseconds in all UTC days (no DST)
   const oneDay = 1000 * 60 * 60 * 24;
 
   // A day in UTC always lasts 24 hours (unlike in other time formats)
-  const start = Date.UTC(formattedEndDate.getFullYear(), formattedEndDate.getMonth(), formattedEndDate.getDate());
-  const end = Date.UTC(formattedStartDate.getFullYear(), formattedStartDate.getMonth(), formattedStartDate.getDate());
+  const start = Date.UTC(
+    formattedEndDate.getFullYear(),
+    formattedEndDate.getMonth(),
+    formattedEndDate.getDate()
+  );
+  const end = Date.UTC(
+    formattedStartDate.getFullYear(),
+    formattedStartDate.getMonth(),
+    formattedStartDate.getDate()
+  );
 
   // so it's safe to divide by 24 hours
   let daysBetween = (start - end) / oneDay;
   return daysBetween;
 }
-
 
 function renderTravelerTripRequestForm() {
   getAllTripsForAgent().then(function (result) {
@@ -93,18 +105,25 @@ function renderTravelerTripRequestForm() {
       let i;
       let destinations = [];
 
-      for (i = 0; i < result.length; i++) { destinations.push(destinationsArr[i]) };
-      let searchBarLabel = document.createElement('label');
-      searchBarLabel.setAttribute('for', 'destination-choice');
-      let searchBarLabelText = document.createTextNode('Search for a Destination:')
+      for (i = 0; i < result.length; i++) {
+        destinations.push(destinationsArr[i]);
+      }
+      let searchBarLabel = document.createElement("label");
+      searchBarLabel.setAttribute("for", "destination-choice");
+      let searchBarLabelText = document.createTextNode(
+        "Search for a Destination:"
+      );
       searchBarLabel.appendChild(searchBarLabelText);
-      document.querySelectorAll('section')[2].setAttribute('id', 'request-form');
-      document.querySelectorAll('section')[2].setAttribute('class', 'container');
-      document.getElementById('request-form').appendChild(searchBarLabel);
+      document
+        .querySelectorAll("section")[2]
+        .setAttribute("id", "request-form");
+      document
+        .querySelectorAll("section")[2]
+        .setAttribute("class", "container");
+      document.getElementById("request-form").appendChild(searchBarLabel);
       let inputList = document.createElement("input");
       inputList.setAttribute("list", "destination-choice");
       inputList.setAttribute("id", "destination");
-      // inputList.setAttribute("name", "destination-choice");
       document.getElementById("request-form").appendChild(inputList);
       let dataList = document.createElement("datalist");
       dataList.setAttribute("id", "destination-choice");
@@ -117,124 +136,161 @@ function renderTravelerTripRequestForm() {
         document.getElementById("destination-choice").appendChild(option);
       }
 
-      let numberOfTravelersLabel = document.createElement('label');
-      numberOfTravelersLabel.setAttribute('for', 'number-of-travelers');
-      let numberOfTravelersLabelText = document.createTextNode('Number of Travelers:')
+      let numberOfTravelersLabel = document.createElement("label");
+      numberOfTravelersLabel.setAttribute("for", "number-of-travelers");
+      let numberOfTravelersLabelText = document.createTextNode(
+        "Number of Travelers:"
+      );
       numberOfTravelersLabel.appendChild(numberOfTravelersLabelText);
-      document.getElementById('request-form').appendChild(numberOfTravelersLabel);
+      document
+        .getElementById("request-form")
+        .appendChild(numberOfTravelersLabel);
       let numberOfTravelersInput = document.createElement("input");
       numberOfTravelersInput.setAttribute("list", "number-of-travelers");
       numberOfTravelersInput.setAttribute("id", "number-of-travs");
-      document.getElementById("request-form").appendChild(numberOfTravelersInput);
+      document
+        .getElementById("request-form")
+        .appendChild(numberOfTravelersInput);
       let numberOfTravelersDataList = document.createElement("datalist");
       numberOfTravelersDataList.setAttribute("id", "number-of-travelers");
-      document.getElementById("request-form").appendChild(numberOfTravelersDataList);
+      document
+        .getElementById("request-form")
+        .appendChild(numberOfTravelersDataList);
       for (i = 1; i <= 20; i++) {
         let number = document.createElement("option");
         number.setAttribute("value", `${i}`);
-        document.getElementById('number-of-travelers').appendChild(number);
+        document.getElementById("number-of-travelers").appendChild(number);
       }
 
-      let tripDurationLabel = document.createElement('label');
-      tripDurationLabel.setAttribute('for', 'trip-duration');
-      let tripDurationLabelText = document.createTextNode('Please select a start and end date:')
+      let tripDurationLabel = document.createElement("label");
+      tripDurationLabel.setAttribute("for", "trip-duration");
+      let tripDurationLabelText = document.createTextNode(
+        "Please select a start and end date:"
+      );
       tripDurationLabel.appendChild(tripDurationLabelText);
-      document.getElementById('request-form').appendChild(tripDurationLabel);
+      document.getElementById("request-form").appendChild(tripDurationLabel);
 
-      let tripStartLabel = document.createElement('label');
-      tripStartLabel.setAttribute('for', 'trip-start');
-      document.getElementById('request-form').appendChild(tripStartLabel);
-      let startDateInput = document.createElement("input")
+      let tripStartLabel = document.createElement("label");
+      tripStartLabel.setAttribute("for", "trip-start");
+      document.getElementById("request-form").appendChild(tripStartLabel);
+      let today = new Date().toLocaleDateString();
+      let startDateInput = document.createElement("input");
       startDateInput.setAttribute("type", "date");
       startDateInput.setAttribute("id", "start");
-      startDateInput.setAttribute("name", "trip-start")
-      startDateInput.setAttribute("value", "2020-09-27")
-      startDateInput.setAttribute("min", "2020-09-27")
-      startDateInput.setAttribute("max", "2021-12-31")
-      document.getElementById('request-form').appendChild(startDateInput);
+      startDateInput.setAttribute("name", "trip-start");
+      startDateInput.setAttribute("value", `${today}`);
+      startDateInput.setAttribute("min", `${today}`);
+      startDateInput.setAttribute("max", "2021-12-31");
+      document.getElementById("request-form").appendChild(startDateInput);
 
-      let tripEndLabel = document.createElement('label');
-      tripEndLabel.setAttribute('for', 'trip-end');
-      document.getElementById('request-form').appendChild(tripEndLabel);
-      let endDateInput = document.createElement("input")
+      let tripEndLabel = document.createElement("label");
+      tripEndLabel.setAttribute("for", "trip-end");
+      document.getElementById("request-form").appendChild(tripEndLabel);
+      let endDateInput = document.createElement("input");
       endDateInput.setAttribute("type", "date");
       endDateInput.setAttribute("id", "end");
       endDateInput.setAttribute("name", "trip-end");
-      endDateInput.setAttribute("value", "2020-09-27");
-      endDateInput.setAttribute("min", "2020-09-27");
+      endDateInput.setAttribute("value", `${today}`);
+      endDateInput.setAttribute("min", `${today}`);
       endDateInput.setAttribute("max", "2023-12-31");
-      document.getElementById('request-form').appendChild(endDateInput);
+      document.getElementById("request-form").appendChild(endDateInput);
 
-      let tripRequestButton = document.createElement('button');
+      let tripRequestButton = document.createElement("button");
       tripRequestButton.setAttribute("id", "trip-request-button");
-      tripRequestButton.textContent = 'CALCULATE';
+      tripRequestButton.textContent = "CALCULATE";
       tripRequestButton.onclick = function calculateTripRequest() {
         let tripRequestData = [];
-        let tripRequestDestination = document.getElementById('destination').value;
-        let numberOfTravelers = document.getElementById('number-of-travs').value;
-        let tripRequestStartDate = document.getElementById('start').value;
-        let tripRequestEndDate = document.getElementById('end').value;
-        tripRequestData.push("Destination: " + tripRequestDestination, "Number of Travelers: " + numberOfTravelers, "Trip Start Date: " + tripRequestStartDate, "Trip End Date:" + tripRequestEndDate);
+        let tripRequestDestination = document.getElementById("destination")
+          .value;
+        let numberOfTravelers = document.getElementById("number-of-travs")
+          .value;
+        let tripRequestStartDate = document.getElementById("start").value;
+        let tripRequestEndDate = document.getElementById("end").value;
+        tripRequestData.push(
+          "Destination: " + tripRequestDestination,
+          "Number of Travelers: " + numberOfTravelers,
+          "Trip Start Date: " + tripRequestStartDate,
+          "Trip End Date:" + tripRequestEndDate
+        );
         console.log(tripRequestData, allTrips, destinations);
-        let i; for (i = 0; i < destinations.length; i++) {
+        let i;
+        for (i = 0; i < destinations.length; i++) {
           if (tripRequestDestination === destinations[i].destination) {
             let destinationInfo = destinations[i];
             console.log(destinationInfo);
             daysBetween(tripRequestStartDate, tripRequestEndDate);
-            let flightCosts = numberOfTravelers * destinations[i].estimatedFlightCostPerPerson;
-            let lodgingCostsPerDay = numberOfTravelers * destinations[i].estimatedLodgingCostPerDay;
-            let totalLodgingCosts = lodgingCostsPerDay * daysBetween(tripRequestStartDate, tripRequestEndDate);
+            let flightCosts =
+              numberOfTravelers * destinations[i].estimatedFlightCostPerPerson;
+            let lodgingCostsPerDay =
+              numberOfTravelers * destinations[i].estimatedLodgingCostPerDay;
+            let totalLodgingCosts =
+              lodgingCostsPerDay *
+              daysBetween(tripRequestStartDate, tripRequestEndDate);
             let costBeforeAgentFee = flightCosts + totalLodgingCosts;
             let agentFee = costBeforeAgentFee * 0.1;
             let totalTripCost = costBeforeAgentFee + agentFee;
-            document.getElementById('request-form').style.display = "none";
+            document.getElementById("request-form").style.display = "none";
             renderTripsHeader("Your Requested");
-            document.querySelectorAll('section')[3].setAttribute('id', 'requested-trips')
+            document
+              .querySelectorAll("section")[3]
+              .setAttribute("id", "requested-trips");
             let destinationImage = destinationInfo.image;
             var img = document.createElement("img");
             img.className = "destination-image";
             img.src = `${destinationImage}`;
-            document.querySelectorAll('section')[3].appendChild(img);
+            document.querySelectorAll("section")[3].appendChild(img);
 
-            let destinationName = document.createTextNode(`Trip Destination: ${destinationInfo.destination}`)
+            let destinationName = document.createTextNode(
+              `Trip Destination: ${destinationInfo.destination}`
+            );
 
-            let destinationNameParagraph = document.createElement('paragraph');
+            let destinationNameParagraph = document.createElement("paragraph");
             destinationNameParagraph.appendChild(destinationName);
-            document.querySelectorAll('section')[3].appendChild(destinationNameParagraph);
+            document
+              .querySelectorAll("section")[3]
+              .appendChild(destinationNameParagraph);
 
-            let numOfTravelersNode = document.createTextNode(`Number of Travelers: ${numberOfTravelers}`);
-            let numOfTravelersParagraph = document.createElement('paragraph');
+            let numOfTravelersNode = document.createTextNode(
+              `Number of Travelers: ${numberOfTravelers}`
+            );
+            let numOfTravelersParagraph = document.createElement("paragraph");
             numOfTravelersParagraph.appendChild(numOfTravelersNode);
-            document.querySelectorAll('section')[3].appendChild(numOfTravelersParagraph);
+            document
+              .querySelectorAll("section")[3]
+              .appendChild(numOfTravelersParagraph);
 
-            let formattedStartDate = new Date(tripRequestStartDate).toLocaleDateString();
-            let formattedEndDate = new Date(tripRequestEndDate).toLocaleDateString();
-            let startDateParagraph = document.createElement('paragraph');
-            let endDateParagraph = document.createElement('paragraph');
-            let startDateNode = document.createTextNode(`Trip Start Date: ${formattedStartDate}`);
-            let endDateNode = document.createTextNode(`Trip End Date: ${formattedEndDate}`)
+            let formattedStartDate = new Date(tripRequestStartDate);
+            formattedStartDate = new Date(formattedStartDate.getTime() + formattedStartDate.getTimezoneOffset() * 60000)
+            let formattedEndDate = new Date(tripRequestEndDate);
+            formattedEndDate = new Date(formattedEndDate.getTime() + formattedEndDate.getTimezoneOffset() * 60000)
+
+            let startDateParagraph = document.createElement("paragraph");
+            let endDateParagraph = document.createElement("paragraph");
+            let startDateNode = document.createTextNode(
+              `Trip Start Date: ${formattedStartDate.toDateString()}`
+            );
+            let endDateNode = document.createTextNode(
+              `Trip End Date: ${formattedEndDate.toDateString()}`
+            );
             startDateParagraph.appendChild(startDateNode);
             endDateParagraph.appendChild(endDateNode);
-            document.querySelectorAll('section')[3].appendChild(startDateParagraph);
-            document.querySelectorAll('section')[3].appendChild(endDateParagraph);
+            document
+              .querySelectorAll("section")[3]
+              .appendChild(startDateParagraph);
+            document
+              .querySelectorAll("section")[3]
+              .appendChild(endDateParagraph);
 
-            let totalCostNode = document.createTextNode(`Total Estimated Trip Cost: $${totalTripCost.toFixed(2)}`);
-            document.querySelectorAll('section')[3].appendChild(totalCostNode);
-
-
-
-
-
+            let totalCostNode = document.createTextNode(
+              `Total Estimated Trip Cost: $${totalTripCost.toFixed(2)}`
+            );
+            document.querySelectorAll("section")[3].appendChild(totalCostNode);
           }
-
         }
-
       };
-      document.getElementById('request-form').appendChild(tripRequestButton);
-
-    })
-  })
-
+      document.getElementById("request-form").appendChild(tripRequestButton);
+    });
+  });
 }
 
 function renderTravelerDashboard(travelerDashboardData) {
@@ -247,7 +303,7 @@ function renderTravelerDashboard(travelerDashboardData) {
   renderTripsHeader("Past");
   renderTrips(trips.pastTripsArr, destinations);
   renderTripsHeader("Request");
-  renderTravelerTripRequestForm()
+  renderTravelerTripRequestForm();
 }
 
 function clearLoginForm() {
