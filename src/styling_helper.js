a8b7bc48fd9127e6206ce1fa7118fd3e46a95d97
Joshua Everett <59103702+josh-everett01@users.noreply.github.com>
2020-10-06 11:12:50 -0500

    WIP

---
+++ src/styling_helper.js
@@ -0,0 +1,49 @@
+import * as travel_tracker_service from "./travel_tracker_service"
+import * as dashboard_helper from "./dashboard_helper";
+function styleHomePage() {
+  travel_tracker_service.getAllDestinations().then(function (result) {
+
+    // document.getElementById('site-container').style.display = "none";
+    document.getElementById('login-form').style.visibility = "initial"
+    "initial"
+
+    let destinations = result;
+    let homePage = document.createElement('div');
+    homePage.className = "home-page";
+    homePage.id = "home-page";
+    let homePageH1 = document.createElement('h1');
+    homePageH1.id = "home-page-h1";
+    let h1Text = document.createTextNode('Where would you like to go?')
+    homePageH1.appendChild(h1Text)
+    document.getElementById('test-id').appendChild(homePageH1)
+    document.getElementById('test-id').appendChild(homePage)
+    for (let i = 0; i < destinations.length; i++) {
+      let imageAndNameDiv = document.createElement('div');
+      imageAndNameDiv.className = "image-and-name-div";
+      let destinationImage = destinations[i].image;
+      var img = document.createElement("img");
+      img.className = "home-destination-image";
+      img.src = `${destinationImage}`;
+
+
+
+      let destinationName = destinations[i].destination;
+      let paragraph = document.createElement('paragraph');
+      paragraph.className = "home-destination-name";
+      let destinationNode = document.createTextNode(`${destinationName}`);
+      paragraph.appendChild(destinationNode)
+      imageAndNameDiv.appendChild(img)
+      imageAndNameDiv.appendChild(paragraph)
+      document.getElementById('home-page').appendChild(imageAndNameDiv)
+    }
+
+
+  }
+  )
+
+
+}
+
+
+
+export { styleHomePage }
